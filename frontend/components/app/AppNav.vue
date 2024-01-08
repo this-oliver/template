1be80ce59f02191f389489dc3c725b555c0444ba
Oliver Manzi <olivermanzi.dev@gmail.com>
2024-01-08 08:06:27 +0100

    (v0.7.0) feature: adds order cart
    - add/remove products from cart
    - track real-time (local) product quantity
    - view cart

--- frontend/components/app/AppNav.vue
+++ frontend/components/app/AppNav.vue
@@ -1,13 +1,17 @@
 <script setup lang="ts">
 import { useDisplay } from 'vuetify';
-import { useNavigationStore } from '~/stores/app';
-import { useSidebarStore } from '~/stores/app';
+import { useNavigationStore, useSidebarStore } from '~/stores/app';
 import { useAuthStore } from '~/stores/auth';
+import { useOrderStore } from '~/stores/shop';
+import type { ActionItem } from '~/components/base/BaseCard.vue';
 
 const { smAndDown } = useDisplay();
 const auth = useAuthStore();
 const drawer = useSidebarStore();
 const navigation = useNavigationStore();
+const order = useOrderStore();
+
+const options = computed<ActionItem[]>(() => smAndDown.value ? [] : navigation.options);
 </script>
 
 <template>
@@ -31,21 +35,26 @@ const navigation = useNavigationStore();
 
     <v-spacer />
 
-    <div v-if="!smAndDown">
-      <base-btn
-        v-for="option in navigation.options"
-        :key="option.label"
-        class="mx-1 plain"
-        plain
-        :color="option.color"
-        :to="option.to"
-        @click="option.action"
-      >
-        {{ option.label }}
-      </base-btn>
+    <base-btn
+      v-for="option in options"
+      :key="option.label"
+      class="mx-1 plain"
+      :color="option.color"
+      :to="option.to"
+      @click="option.action"
+    >
+      {{ option.label }}
+    </base-btn>
 
-      <app-admin-menu v-if="auth.isAuthenticated" />
-    </div>
+    <cart-btn
+      v-if="!smAndDown && order.totalCartItems > 0"
+      class="mx-1"
+    />
+
+    <app-admin-menu
+      v-if="!smAndDown && auth.isAuthenticated"
+      class="mx-1"
+    />
   </v-app-bar>
 </template>
 
