7601a02e631d3abbc749377227ff8fbe81a5871f
Oliver Manzi <olivermanzi.dev@gmail.com>
2024-01-16 07:56:34 +0100

    (v0.8.2) patch: improves cart ui
    - adds cart btn to mobile screens
    - structures the cart sidebar layout
    - renames CartCard to CartSidebarCart

--- frontend/components/cards/CartSidebarCard.vue
+++ frontend/components/cards/CartSidebarCard.vue
@@ -15,8 +15,10 @@ function getThumbnail(product: Product): Image {
 <template>
   <v-navigation-drawer
     v-model="orderStore.showCart"
+    class="pa-2"
     temporary
     location="right"
+    width="400"
   >
     <template #prepend>
       <h1 class="ma-1">
@@ -29,15 +31,41 @@ function getThumbnail(product: Product): Image {
         v-for="item in orderStore.cart"
         :key="item.product._id"
         :prepend-avatar="getThumbnail(item.product).src"
-        :title="item.product.name"
-        :subtitle="`${item.quantity} x ${item.product.price} ${productStore.currency}`"
-      />
+      >
+        <div class="list-item-layout">
+          <div>{{ item.product.name }}</div>
+          <div>{{ `${item.quantity} x ${item.product.price} ${productStore.currency}` }}</div>
+        </div>
+      </v-list-item>
+
+      <v-divider class="mt-2" />
+
+      <v-list-item class="mt-2">
+        <div class="list-item-layout">
+          <div>Total</div>
+          <div style="font-weight: bold;">
+            {{ orderStore.totalCartCost }} {{ productStore.currency }}
+          </div>
+        </div>
+      </v-list-item>
     </v-list>
 
     <template #append>
-      <h3 class="ma-1">
-        Total: {{ orderStore.totalCartCost }} {{ productStore.currency }}
-      </h3>
+      <base-btn
+        class="ma-1"
+        color="success"
+        size="large"
+        block
+      >
+        Checkout
+      </base-btn>
     </template>
   </v-navigation-drawer>
 </template>
+
+<style scoped>
+.list-item-layout {
+  display: flex; /* Ensures flexbox is used */
+  justify-content: space-between; /* Aligns children at each end */
+}
+</style>
