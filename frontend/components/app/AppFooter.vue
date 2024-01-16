b291baf7b19f6a4906e138bc9c37b2880b846161
Oliver Manzi <olivermanzi.dev@gmail.com>
2024-01-16 08:20:42 +0100

    (v0.8.3) patch: improves footer
    - moves features link from nav bar to footer
    - adds css for spacing out links

--- frontend/components/app/AppFooter.vue
+++ frontend/components/app/AppFooter.vue
@@ -1,13 +1,56 @@
 <script setup lang="ts">
+type Link = {label: string, url: string};
+const internalLinks: Link[] = [
+	{ label: 'Features', url: '/features' }
+];
+const externalLinks: Link[] = [
+	{ label: 'GitHub', url: 'https://github.com/this-oliver/template' }
+];
 </script>
 
 <template>
-  <v-footer app>
-    <p>
+  <v-footer
+    id="app-footer"
+    app
+  >
+    <div>
+      <nuxt-link
+        v-for="link in internalLinks"
+        :key="link.url"
+        :to="link.url"
+        class="mx-1"
+      >
+        {{ link.label }}
+      </nuxt-link>
+      <a
+        v-for="link in externalLinks"
+        :key="link.url"
+        :href="link.url"
+        target="_blank"
+        class="mx-1"
+      >
+        {{ link.label }}
+      </a>
+    </div>
+
+    <div>
       template made by <a
         href="www.oliverrr.net"
         target="_blank"
       >oliverrr 🤠</a>
-    </p>
+    </div>
   </v-footer>
 </template>
+
+<style scoped>
+#app-footer {
+  display: flex;
+  justify-content: space-between;
+}
+
+@media (max-width: 600px) {
+  #app-footer {
+    flex-direction: column;
+  }
+}
+</style>
