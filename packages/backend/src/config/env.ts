ccb29f7f961104c0c0e829f678cc723983d3966d
Oliver Manzi <olivermanzi.dev@gmail.com>
2024-05-23 19:22:19 +0200

    (v1.0.4) patch: backend - sets default port to 5000

--- packages/backend/src/config/env.ts
+++ packages/backend/src/config/env.ts
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV === "test") {
 
 // General config
 const NODE_ENV = process.env.NODE_ENV || "prod";
-const PORT = process.env.PORT || "3000";
+const PORT = process.env.PORT || "5000";
 const DATABASE_URL: string = process.env.DATABASE_URL as string;
 
 // Security config
